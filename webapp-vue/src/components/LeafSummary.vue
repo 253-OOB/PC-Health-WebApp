529c4eb461791029f61c74cf9e14a03eb9240179
elmarwanos <marwanbassam13@gmail.com>
2021-03-04 21:56:46 +0200

    Create Leaf prefab

---
+++ webapp-vue/src/components/LeafSummary.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="leaf-wrapper">
+    <span id="title" v-for="title in metrics" v-bind:key="title">{{ title }}</span>
+    <div id="leaf-info">Tags ON</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeafSummary",
+  props: {
+      metrics: String
+  },
+};
+</script>
+
+<style scoped>
+#leaf-wrapper {
+  border: solid black 1px;
+  width: fit-content;
+  height: fit-content;
+  text-align: start;
+  margin: 30px;
+}
+
+#title {
+  font-size: 17px;
+  font-weight: bold;
+}
+
+#leaf-info {
+  padding: 10px 7px 2px 15px;
+  background-color: grey;
+}
+</style>
--- webapp-vue/src/views/Overview.vue
+++ webapp-vue/src/views/Overview.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="overview">
+  <div id="main">
     <h1>Display all leafs here</h1>
+    <LeafSummary v-bind:metrics="metrics[1]" />
+    <component v-bind:is="cmpName"/>
   </div>
 </template>
 
 <script>
+import LeafSummary from '../components/LeafSummary'
 
+export default {
+  name: "Overview",
+  data() {
+    return{
+      metrics: ['Leaf1', 'Leaf2', 'x', 'e'],
+      cmpName: ''
+    }
+  },
+  components: {
+    LeafSummary,
+  },
+  methods: {
+    createLeafSummarys() {
+      for (let index = 0; index < this.metrics.length; index++) {
+        this.cmpName = 'LeafSummary';
+      }
+    }
+  },
+  mounted() {
+    this.createLeafSummarys();
+  }
+};
 </script>
 
 <style scoped>
