0182e4e067e25e8e4433d368c613dc12c4ad9969
elmarwanos <marwanbassam13@gmail.com>
2021-03-04 23:29:41 +0200

    Position the LeafSummaries in a responsive grid

--- webapp-vue/src/views/Overview.vue
+++ webapp-vue/src/views/Overview.vue
@@ -1,37 +1,78 @@
 <template>
   <div id="main">
-    <h1>Display all leafs here</h1>
-    <LeafSummary v-bind:metrics="metrics[1]" />
-    <component v-bind:is="cmpName"/>
+    <component
+      v-for="(component, index) in componentLists"
+      :key="index"
+      :is="component.name"
+      v-bind:metrics="component.title"
+    ></component>
   </div>
 </template>
 
 <script>
-import LeafSummary from '../components/LeafSummary'
+import LeafSummary from "../components/LeafSummary";
 
 export default {
   name: "Overview",
   data() {
-    return{
-      metrics: ['Leaf1', 'Leaf2', 'x', 'e'],
-      cmpName: ''
-    }
+    return {
+      componentLists: [
+        {
+          name: "LeafSummary",
+          title: "Leaf 1",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 2",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 3",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 4",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 5",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 6",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 7",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 8",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 9",
+        },
+        {
+          name: "LeafSummary",
+          title: "Leaf 10",
+        },
+      ],
+    };
   },
   components: {
     LeafSummary,
   },
-  methods: {
-    createLeafSummarys() {
-      for (let index = 0; index < this.metrics.length; index++) {
-        this.cmpName = 'LeafSummary';
-      }
-    }
-  },
-  mounted() {
-    this.createLeafSummarys();
-  }
 };
 </script>
 
 <style scoped>
+#main {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 200px));
+  grid-template-rows: repeat(auto-fill, 100px);
+  justify-items: center;
+  align-items: center;
+}
+
 </style>
