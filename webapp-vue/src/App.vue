d62febd23bca7c38bf4f3efac5efc1b680247d79
elmarwanos <marwanbassam13@gmail.com>
2021-03-04 13:00:15 +0200

    Add Header

---
+++ webapp-vue/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Header />
+    <MainContent />
+  </div>
+</template>
+
+<script>
+import Header from './components/layouts/Header';
+import MainContent from './components/layouts/MainContent';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    MainContent,
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  text-align: center;
+  color: #FFFFFF;
+  background-color: #F7F7F7;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
---
+++ webapp-vue/src/components/layouts/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h1>PC Health</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header{
+    background-color: grey;
+}
+</style>
