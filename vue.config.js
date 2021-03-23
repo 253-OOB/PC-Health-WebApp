01361b9e7c9f9b93d702ebc1113943eca757f592
florardu <andretabourian@hotmail.com>
2021-03-23 19:16:57 +0200

    Fixed a bug

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,35 @@
-
+// module.exports = {
+//   
+// };
 
 module.exports = {
+  
+  publicPath: '',
+  
   chainWebpack: (config) => {
-  config.module
-    .rule('images')
-    .use('url-loader')
-    .tap(options => Object.assign({}, options, { name: '[name].[ext]' }));
-},
-css: {
-  extract: {
-    filename: '[name].css',
-    chunkFilename: '[name].css',
+
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .tap(options => Object.assign({}, options, { name: '[name].[ext]' }));
+
   },
-},
-configureWebpack: {
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[name].js',
+  
+  css: {
+
+    extract: {
+      filename: '[name].css',
+      chunkFilename: '[name].css',
+    }
+
+  },
+  
+  configureWebpack: {
+
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+    }
+
   }
-}
 };
