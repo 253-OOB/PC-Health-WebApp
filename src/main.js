cb946bce7c4ab16e83fe402f4d6f5316d33f96d6
elmarwanos <marwanbassam13@gmail.com>
2021-03-29 23:41:46 +0300

    Ordered the api calls and app loading

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+////////////////////////////////////
+//////////// LIBRARIES /////////////
+////////////////////////////////////
+import dotenv from "dotenv";
+dotenv.config();
+
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";  // Router
+import router from "./router"; // Router
 
 // FontAwesomeIcon
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -17,15 +23,43 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-Vue.use(BootstrapVue); 
+Vue.use(BootstrapVue);
 
 // CoreuiChartjsVue
-import CoreuiVueCharts from '@coreui/vue-chartjs'
-Vue.use(CoreuiVueCharts)
+import CoreuiVueCharts from "@coreui/vue-chartjs";
+Vue.use(CoreuiVueCharts);
 
+////////////////////////////////////
+///////////// GLOBALS //////////////
+////////////////////////////////////
+let graphData = null;
+
+////////////////////////////////////
+///////////// METHODS //////////////
+////////////////////////////////////
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+// method to make http get requests
+async function getReq(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+async function launchVueApp() {
+  // Put calls that need to be dont before anything HERE
+  await getReq(process.env.VUE_APP_API_GET_MEMORY_RE).then((response) => {
+    graphData = response;
+    console.log("Fetched memory data from API");
+    Vue.prototype.$graphData = graphData;
+  });
+
+  // Loads in vue app
+  new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount("#app");
+}
+
+
+launchVueApp();
