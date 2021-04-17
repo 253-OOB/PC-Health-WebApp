feb2f8860ff1c8ba15e1616f1b678ed0291f7f61
elmarwanos <marwanbassam13@gmail.com>
2021-04-17 03:51:16 +0300

    Added basic tabs to notification page

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,74 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-const TheContainer = () => import('../layouts/TheContainer.vue')
+const TheContainer = () => import("../layouts/TheContainer.vue");
 // const Page404 = () => import('../views/pages/Page404.vue')
 
-const Overview = () => import('../views/Overview.vue')
-const Details = () => import('../views/Details.vue')
+const Overview = () => import("../views/Overview.vue");
+const Details = () => import("../views/Details.vue");
 const Graphs = () => import("../views/Graphs.vue");
 const Notifications = () => import("../views/Notifications.vue");
 const Settings = () => import("../views/Settings.vue");
 
-const Login = () => import('../pages/Login.vue');
+const Login = () => import("../pages/Login.vue");
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 export default new VueRouter({
-    routes: [
+  routes: [
+    {
+      path: "/",
+      redirect: "/overview",
+      name: "TheContainer",
+      component: TheContainer,
+      meta: {
+        requiresAuth: true,
+      },
+      children: [
         {
-            path: "/",
-            redirect: "/overview",
-            name: "TheContainer",
-            component: TheContainer,
-            meta: {
-                requiresAuth: true
-            },
-            children: [
-                {
-                    path: "overview",
-                    name: "Overview",
-                    component: Overview,
-                },
-                {
-                    path: "details",
-                    name: "Details",
-                    component: Details,
-                },
-                {
-                    path: "graphs",
-                    name: "Graphs",
-                    component: Graphs,
-                },
-                {
-                    path: "notifications",
-                    name: "Notifications",
-                    component: Notifications,
-                },
-                {
-                    path: "settings",
-                    name: "Settings",
-                    component: Settings,
-                }
-            ]
+          path: "overview",
+          name: "Overview",
+          component: Overview,
         },
         {
-            path: '/login',
-            name: 'Login',
-            component: Login,
-            meta: {
-                
-            }
-        }
-    ],
+          path: "details",
+          name: "Details",
+          component: Details,
+        },
+        {
+          path: "graphs",
+          name: "Graphs",
+          component: Graphs,
+        },
+        {
+          path: "notifications",
+          name: "Notifications",
+          component: Notifications,
+          children: [
+            {
+              path: "overview",
+              name: "Overview",
+              component: Overview,
+            },
+            {
+              path: "details",
+              name: "Details",
+              component: Details,
+            },
+          ],
+        },
+        {
+          path: "settings",
+          name: "Settings",
+          component: Settings,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+      meta: {},
+    },
+  ],
 });
-
