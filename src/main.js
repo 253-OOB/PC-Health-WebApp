df52393b33456035a007cb9b0fa5a7814743fc98
elmarwanos <marwanbassam13@gmail.com>
2021-04-17 17:55:42 +0300

    Modulated call functions

--- src/main.js
+++ src/main.js
@@ -11,7 +11,13 @@ import router from "./router"; // Router
 // FontAwesomeIcon
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faInfoCircle, faTh, faChartLine, faBell, faCog } from "@fortawesome/free-solid-svg-icons";
+import {
+    faInfoCircle,
+    faTh,
+    faChartLine,
+    faBell,
+    faCog,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(faInfoCircle, faTh, faChartLine, faBell, faCog);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
@@ -30,10 +36,12 @@ Vue.use(CoreuiVueCharts);
 ///////////// GLOBALS //////////////
 ////////////////////////////////////
 let graphData = new Object(); //dictionary for the graph data
+let tags = new Object();
+let organizations = new Object();
 
 let session = {
     LoggedIn: false,
-    RefreshToken: undefined
+    RefreshToken: undefined,
 };
 
 ////////////////////////////////////
@@ -43,67 +51,114 @@ Vue.config.productionTip = false;
 
 // method to make http get requests
 async function getReq(url) {
-
     let response = await fetch(url);
     let data = await response.json();
     return data;
-
 }
 
 // Configure Route Permissions
-router.beforeEach( (to, from, next) => {
-    
-    if ( to.matched.some(record => record.meta.requiresAuth) ) {
-
-        if ( ! session.LoggedIn ) {
-
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (!session.LoggedIn) {
             next({
-                path: '/login',
-                query: {redirect: to.fullPath}
+                path: "/login",
+                query: { redirect: to.fullPath },
             });
-
         } else {
             next();
         }
-
     } else {
         next();
     }
-
 });
 
+async function callRequests() {
+    // Put calls that need to be done before anything HERE
+
+    async function getCPU() {
+        // CPU Call 
+        await getReq(process.env.VUE_APP_API_GET_CPU_RE)
+            .then((response) => {
+                graphData["cpu"] = response;
+                console.log("Fetched cpu data from API");
+                Vue.prototype.$graphData = graphData;
+            })
+            .catch((err) => {
+                console.log("Error Occured fetching cpu from API:\n"+err);
+            });
+    }
+
+    async function getMemory() {
+        // Memory Call
+        await getReq(process.env.VUE_APP_API_GET_MEMORY_RE)
+            .then((response) => {
+                graphData["memory"] = response;
+                console.log("Fetched memory data from API");
+                Vue.prototype.$graphData = graphData;
+            })
+            .catch((err) => {
+                console.log("Error Occured fetching memory from API:\n"+err);
+            });
+    }
+
+    async function getDisk() {
+        // Disk Call
+        await getReq(process.env.VUE_APP_API_GET_LOGIC_DISK_RE)
+            .then((response) => {
+                graphData["disk"] = response;
+                console.log("Fetched disk data from API");
+                Vue.prototype.$graphData = graphData;
+            })
+            .catch((err) => {
+                console.log("Error Occured fetching disk from API:\n"+err);
+            });
+    }
+
+    async function getTags() {
+        // Tags Call
+        await getReq(process.env.VUE_APP_API_GET_TAGS)
+            .then((response) => {
+                tags = response;
+                console.log("Fetched tags from API");
+                Vue.prototype.$tags = tags;
+            })
+            .catch((err) => {
+                console.log("Error Occured fetching tags from API:\n"+err);
+            });
+    }
+
+    async function getOrgs() {
+        // Orgs Call
+        await getReq(process.env.VUE_APP_API_GET_ORGS)
+            .then((response) => {
+                organizations = response;
+                console.log("Fetched organizations from API");
+                Vue.prototype.$organizations = organizations;
+            })
+            .catch((err) => {
+                console.log("Error Occured fetching organizations from API:\n"+err);
+            });
+    }
+
+
+    // getCPU();
+    // getMemory();
+    // getDisk();
+    // getTags();
+    // getOrgs();
+}
+
 async function launchVueApp() {
-  // Put calls that need to be dont before anything HERE
-
     Vue.prototype.$session = session;
-    
-    // CPU Call
-    await getReq(process.env.VUE_APP_API_GET_CPU_RE).then((response) => {
-        graphData["cpu"] = response;
-        console.log("Fetched cpu data from API");
-        Vue.prototype.$graphData = graphData;
-    });
 
-    // Memory Call
-    await getReq(process.env.VUE_APP_API_GET_MEMORY_RE).then((response) => {
-        graphData["memory"] = response;
-        console.log("Fetched memory data from API");
-        Vue.prototype.$graphData = graphData;
-    });
-
-    // Disk Call
-    await getReq(process.env.VUE_APP_API_GET_LOGIC_DISK_RE).then((response) => {
-        graphData["disk"] = response;
-        console.log("Fetched disk data from API");
-        Vue.prototype.$graphData = graphData;
-    });
+    //Makes all api requests
+    callRequests();
 
     // Loads in vue app
     new Vue({
         router,
         render: (h) => h(App),
     }).$mount("#app");
-
 }
 
 launchVueApp();
