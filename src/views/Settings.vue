43f486cb96b11e15946a7b7e0a0170d908fc7697
elmarwanos <marwanbassam13@gmail.com>
2021-04-18 17:23:15 +0300

    Added button to install leaf app in Settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="settings">
     Coming Soon
+    <button v-on:click="installApp()">Install App</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    installApp() {
+      window.open(process.env.VUE_APP_API_INSTALL, "_blank");
+    },
+  },
+};
+
 </script>
 
 <style></style>
