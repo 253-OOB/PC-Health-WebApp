fa16d4003591eac3d1dfbbef35a348d16784be4a
elmarwanos <marwanbassam13@gmail.com>
2021-04-24 06:00:24 +0300

    Linked tag create endpoint to leaf modal menu

--- src/components/LeafSummary.vue
+++ src/components/LeafSummary.vue
@@ -1,40 +1,77 @@
 <template>
-    <div
-        @click="showModal"
-        @contextmenu.prevent="$refs.menu.open"
-        @click.right="checkIfTagsExist"
-        id="leaf-wrapper"
-    >
-        <div class="title flex-aligned">
-            <span>{{ metrics.AssignedName }}</span>
-        </div>
-        <div class="leaf-tags-grp">
-            <div
-                class="leaf-tag"
-                v-for="(tag, index) in assignedTagNames"
-                :key="index"
-            >
-                {{ tag }}
+    <div class="LeafSummary">
+        <div
+            @click="showModal"
+            @contextmenu.prevent="$refs.menu.open"
+            @click.right="checkIfTagsExist"
+            id="leaf-wrapper"
+        >
+            <div class="title flex-aligned">
+                <span>{{ metrics.AssignedName }}</span>
+            </div>
+            <div class="leaf-tags-grp">
+                <div
+                    class="leaf-tag"
+                    v-for="(tag, index) in assignedTagNames"
+                    :key="index"
+                >
+                    {{ tag }}
+                </div>
             </div>
         </div>
 
         <!--                               -->
         <!-- Modal for Notification Config -->
         <!--                               -->
-        <b-modal centered ref="config" :title="modalTitle">
+        <b-modal
+            centered
+            ref="config"
+            :title="modalTitle"
+            @show="resetModal"
+            @hidden="resetModal"
+            @ok="handleOk"
+        >
             <form @submit.stop.prevent="handleSubmit">
                 <!-- Title -->
-                <b-form-group label="Title">
-                    <b-form-input required></b-form-input>
+                <b-form-group
+                    label="Title"
+                    label-for="title-input"
+                    invalid-feedback="Title is required"
+                    :state="titleState"
+                >
+                    <b-form-input
+                        ref="notifTitle"
+                        id="title-input"
+                        v-model="notifTitle"
+                        :state="titleState"
+                        required
+                    ></b-form-input>
                 </b-form-group>
 
                 <!-- Content -->
-                <b-form-group label="Content">
-                    <b-form-input required></b-form-input>
+                <b-form-group
+                    label="Content"
+                    label-for="content-input"
+                    invalid-feedback="Content is required"
+                    :state="contentState"
+                >
+                    <b-form-input
+                        ref="notifContent"
+                        id="content-input"
+                        v-model="notifContent"
+                        :state="contentState"
+                        required
+                    >
+                    </b-form-input>
                 </b-form-group>
 
                 <!-- Dropdowns -->
-                <div class="flex-aligned" style="justify-content: space-around">
+                <div
+                    class="flex-aligned"
+                    style="justify-content: space-around"
+                    invalid-feedback="Value is required"
+                    :state="valueState"
+                >
                     <!-- Metric -->
                     <b-form-select
                         id="metric"
@@ -63,13 +100,19 @@
                     </b-form-select>
                     <!-- Value -->
                     <b-form-input
-                        id="value"
+                        ref="value"
+                        id="value-input"
                         class="modal-dropdown"
                         v-model="valueSelected"
+                        :state="valueState"
+                        required
                         placeholder="-- Value --"
                     >
                     </b-form-input>
                 </div>
+                <p v-if="invalidNotif" style="color: red; text-align: center">
+                    Invalid Information Entered
+                </p>
             </form>
         </b-modal>
 
@@ -82,7 +125,7 @@
                     v-for="(tag, index) in this.$store.state.tags"
                     :key="index"
                     href="#"
-                    @click.prevent="onClick(tag['TagID'])"
+                    @click.prevent="onTagClick(tag['TagID'])"
                 >
                     {{ tag["TagName"] }}</a
                 >
@@ -106,8 +149,17 @@ export default {
     },
     data() {
         return {
-            //Modal Menu
+            // ---------- //
+            // Modal Menu //
+            // ---------- //
+            // Modal meta info
             modalTitle: "",
+            titleState: null,
+            contentState: null,
+            valueState: null,
+            // Modal content
+            notifTitle: "",
+            notifContent: "",
             metricSelected: null,
             metricOptions: [
                 { value: 1, text: "Percent Processor Time (%)" },
@@ -122,13 +174,18 @@ export default {
                 { value: 3, text: "<" },
             ],
             valueSelected: null,
+            invalidNotif: false,
 
-            //Tags
+            // ---- //
+            // Tags //
+            // -----//
             assignedTagNames: [],
         };
     },
     methods: {
-        //Modal Menu
+        // ----------- //
+        // Notif Modal //
+        // ----------- //
         checkOffline(index) {
             let elem = document.getElementsByClassName("title")[index];
             if (!this.metrics.isOnline) {
@@ -139,13 +196,101 @@ export default {
         },
         showModal() {
             this.modalTitle =
-                "Notification Configuration for " + this.metrics.LeafNames;
+                "Notification Configuration for " + this.metrics.AssignedName;
             this.$refs["config"].show();
         },
+        resetModal() {
+            // Modal is reset everytime it opens/closes
+            (this.notifTitle = ""),
+                (this.titleState = null),
+                (this.notifContent = ""),
+                (this.contentState = null),
+                (this.metricSelected = null);
+            this.operatorSelected = null;
+            this.valueSelected = null;
+        },
+        handleOk(bvModalEvt) {
+            // OK -> check if valid ? submit : revert
+            // Prevent modal from closing
+            bvModalEvt.preventDefault();
+            // Trigger submit handler
+            this.handleSubmit();
+        },
+        checkFormValidity() {
+            //check if input is correct
+            const titleValid = this.$refs.notifTitle.checkValidity();
+            const contentValid = this.$refs.notifContent.checkValidity();
+            const valueValid = this.$refs.value.checkValidity();
+            return titleValid && contentValid && valueValid;
+        },
+        handleSubmit() {
+            // decides what to do if the data is valid or invalid
+            if (!this.checkFormValidity()) {
+                // Exit when the form isn't valid
+                return;
+            }
+            fetch(
+                process.env.VUE_APP_CREATE_NOTIF +
+                    "LeafID=" +
+                    this.metrics.LeafID,
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        AccessToken: this.$store.state.AccessToken,
+                        RefreshToken: this.$store.state.RefreshToken,
+                        notification: {
+                            "this.metricSelected": {
+                                Title: this.notifTitle,
+                                Content: this.notifContent,
+                                Comparison: {
+                                    operator: this.operatorSelected,
+                                    value: this.valueSelected,
+                                },
+                            },
+                        },
+                    }),
+                }
+            )
+                .then((response) => {
+                    if (response.status === 201) {
+                        // Notification Creation Succeeded
+                        this.invalidNotif = false;
+                        this.$nextTick(() => {
+                            this.$refs["config"].hide();
+                        });
+                        return;
+                    } else {
+                        this.invalidNotif = true;
+                    }
+                })
+                .catch((err) => {
+                    console.log("Error Creating Notification\n" + err);
+                });
+        },
 
-        // Context Menu
-        onclick(text) {
-            alert(`You clicked ${text}!`);
+        // ------------ //
+        // Context Menu //
+        // ------------ //
+        onTagClick(tag) {
+            const AccToken = this.$store.state.AccessToken;
+            const RefToken = this.$store.state.RefreshToken;
+            const leafID = this.metrics["LeafID"];
+            const tagID = tag;
+            fetch(
+                process.env.VUE_APP_API_ASSIGN_TAG +
+                    "LeafID=" +
+                    leafID +
+                    "&TagID=" +
+                    tagID,
+                {
+                    method: "POST",
+                    body: JSON.stringify({
+                        AccessToken: AccToken,
+                        RefreshToken: RefToken,
+                    }),
+                }
+            );
+            alert(`You clicked ${tag}!`);
         },
         checkIfTagsExist() {
             try {
@@ -163,7 +308,9 @@ export default {
             }
         },
 
-        //Tags
+        // ---- //
+        // Tags //
+        // ---- //
         getAssignedTagNames() {
             try {
                 const tagNames = [];
@@ -198,7 +345,7 @@ export default {
 </script>
 
 <style scoped>
-#leaf-wrapper {
+.LeafSummary {
     border: solid 2px #ced2d8;
     width: 150px;
     height: 70%;
@@ -239,7 +386,7 @@ export default {
 #operator {
     width: 20%;
 }
-#value {
+#value-input {
     width: 20%;
 }
 </style>
