ef8a9a0c2d2efe91dcf4fa7fd266d374e7cbc085
elmarwanos <marwanbassam13@gmail.com>
2021-04-26 13:55:03 +0300

    Integrated Notifications

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -1,11 +1,8 @@
 <template>
     <div class="NotificationItem">
-        <h1>{{ notif.title }}</h1>
-        <p>
-            {{ notif.body }} <br />
-            {{ notif.isAlive }}
-        </p>
-        <p style="text-align: end">{{ notif.timestamp }}</p>
+        <h2>{{ notif.Title }}</h2>
+        <p>{{ notif.Content }} <br /></p>
+        <p class="timestamp">{{ notif.TimeStamp }}</p>
     </div>
 </template>
 
@@ -21,8 +18,11 @@ export default {
 <style scoped>
 .NotificationItem {
     background-color: white;
-    margin: 5px 0px;
-    padding: 5px 10px;
-    border-radius: 50px;
+    margin: 2px 0px;
+    border-radius: 40px;
+}
+.timestamp {
+    text-align: end;
+    margin: 0 25px 0 0;
 }
 </style>
